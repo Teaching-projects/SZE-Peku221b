a04805fd62cb22f73661a81a5f12a2453eed0e6d
Mate Hegyhati <hegyhati.work@gmail.com>
2017-12-20 14:59:40 +0100

    init + egyedkiir

--- GA_POC/poc.c
+++ GA_POC/poc.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
+#define EMBER 4
+#define TULAJDONSAG 5
+
+struct gen{
+    int allel[TULAJDONSAG][EMBER];
+    int megsert;
+};
+
+struct gen kezdetiRandom(){
+    struct gen egyed;
+    int e,t;
+    for(t=0;t<TULAJDONSAG;t++)
+        for(e=0;e<EMBER;e++)
+            egyed.allel[t][e]=e;
+    egyed.megsert=0; // TODO neghivni majd a kiertekelo fuggvenyt.
+    /*
+     * ITt valahogy random beallitgatni az ize-ben levo ertekeket
+     */
+     return egyed;
+}
+
+void egyedKiir(struct gen egyed){
+    int e,t;
+    printf("|");
+    for(t=0;t<TULAJDONSAG;t++){
+        for(e=0;e<EMBER;e++)
+            printf("%d",egyed.allel[t][e]);
+        printf("|");
+    }
+    printf("\n");
+}
+
+    
 int main(){
-    printf("Hello world!");
+    struct gen elso=kezdetiRandom();
+    egyedKiir(elso);
     return 0;
 }
