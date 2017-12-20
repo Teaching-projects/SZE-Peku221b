221c191d0e51a6d3e03d4ca5a7a869d2a986bc6b
Mate Hegyhati <hegyhati.work@gmail.com>
2017-12-20 15:09:09 +0100

    EMBER->SZEK refaktor + tesztek, minta

--- GA_POC/poc.c
+++ GA_POC/poc.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
 
-#define EMBER 4
+#define SZEK 4
 #define TULAJDONSAG 5
 
+/*
+ * 0 - black
+ * 1 - blue
+ * 2 - green
+ * 3 - red
+ *
+ * 0 - Daniel
+ * 1 - Joshua
+ * 2 - Nicholas
+ * 3 - Ryan
+ *
+ * ...
+ */
+ 
 struct gen{
-    int allel[TULAJDONSAG][EMBER];
+    int allel[TULAJDONSAG][SZEK];
     int megsert;
 };
 
+int Teszt1(struct gen egyed) {
+    // Joshua is in one of the ends.
+    if(egyed.allel[1][0]==1 || egyed.allel[1][3]==1) return 0;
+    else return 1;
+}
+
+int Teszt2(struct gen egyed) {
+    // The boy wearning the Black shirt is somewhere to the left of the youngest boy.
+    return 0;
+}
+
 int hanyatSert(struct gen egyed){
+    int sert=0;
+    sert+=Teszt1(egyed);
+    sert+=Teszt2(egyed);
+    // ...
     return 0;
 }
 
 struct gen kezdetiRandom(){
     struct gen egyed;
-    int e,t;
+    int sz,t;
     for(t=0;t<TULAJDONSAG;t++)
-        for(e=0;e<EMBER;e++)
-            egyed.allel[t][e]=e;
+        for(sz=0;sz<SZEK;sz++)
+            egyed.allel[t][sz]=sz;
     egyed.megsert=hanyatSert(egyed); // TODO neghivni majd a kiertekelo fuggvenyt.
     /*
      * ITt valahogy random beallitgatni az ize-ben levo ertekeket
@@ -26,11 +55,11 @@ struct gen kezdetiRandom(){
 }
 
 void egyedKiir(struct gen egyed){
-    int e,t;
+    int sz,t;
     printf("|");
     for(t=0;t<TULAJDONSAG;t++){
-        for(e=0;e<EMBER;e++)
-            printf("%d",egyed.allel[t][e]);
+        for(sz=0;sz<SZEK;sz++)
+            printf("%d",egyed.allel[t][sz]);
         printf("|");
     }
     printf("\n");
