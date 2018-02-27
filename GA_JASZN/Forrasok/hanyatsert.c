f44bb00b1e4b8f70e8d6497117402a84bfa1f8c7
Máté Varga <VargaMate@GitHub.com>
2018-02-27 21:13:59 +0100

    Evolúciós a Zebrára; Egyedkiértékelés; Tesztek
    Összefoglalás hiányzik, nincs ötletem miről lehetne (sokat) írni.

---
+++ GA_JASZN/Forrasok/hanyatsert.c
@@ -0,0 +1,10 @@
+int hanyatSert(struct gen egyed){
+    int sert=0;
+    sert+=Teszt1(egyed);
+    sert+=Teszt2(egyed);
+    sert+=Teszt3(egyed);
+    ...
+    sert+=Teszt11(egyed);
+    sert+=Teszt12(egyed);
+    sert+=Teszt13(egyed);
+    return sert;
