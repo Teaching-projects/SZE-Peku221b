c5c4e63d34117a0b01b2fefcdf461a6aa1492ad7
Mate Hegyhati <hegyhati.work@gmail.com>
2018-03-30 00:23:39 +0200

    tabs -> spaces

--- GA_TDK/poc/Beolvasas/poc.cpp
+++ GA_TDK/poc/Beolvasas/poc.cpp
@@ -48,36 +48,36 @@ int main() {
     ifstream file("egyed.txt");
     if(file.is_open()) {
         
-		//Beolvasás
+        //Beolvasás
         int tulsz,szekek;
         file >> szekek;
         file >> tulsz;
-		string *tulajdonsagok = new string [tulsz];
-		string **egyed = new string * [tulsz];
-		for(int i = 0;i < tulsz;++i) {
+        string *tulajdonsagok = new string [tulsz];
+        string **egyed = new string * [tulsz];
+        for(int i = 0;i < tulsz;++i) {
             file >> tulajdonsagok[i];
             egyed[i] = new string [szekek];
-			for(int j = 0;j < szekek;++j) {
+            for(int j = 0;j < szekek;++j) {
                 file >> egyed[i][j];
-			}
-		}
-		
-		//Kiiratás
-		for(int i =0; i < tulsz; i++){
-			for(int j = 0; j < szekek; ++j){
-				cout << egyed[i][j] << "\t";
-			}
-            cout << "\n";
-		}
-		for(int j = 0; j < tulsz; ++j)
-            cout << tulajdonsagok[j] << "\t";
-		
-        //Felszabadítás
-		delete[] tulajdonsagok;
-		for(int i = 0;i < tulsz;++i){
-			 delete[] egyed[i]; 
+            }
         }
-		delete[] egyed;
-	}
+        
+        //Kiiratás
+        for(int i =0; i < tulsz; i++){
+            for(int j = 0; j < szekek; ++j){
+                cout << egyed[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+        for(int j = 0; j < tulsz; ++j)
+            cout << tulajdonsagok[j] << "\t";
+        
+        //Felszabadítás
+        delete[] tulajdonsagok;
+        for(int i = 0;i < tulsz;++i){
+             delete[] egyed[i]; 
+        }
+        delete[] egyed;
+    }
 
 }
