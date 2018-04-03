7034c9b46694b7f608269a9c8989648b918f8f8a
szilidaniel <34722259+szilidaniel@users.noreply.github.com>
2018-04-03 14:16:43 +0200

    Randomra probalkozasom
    Egy kis hiba van benne. Nem tudom a peldat miben tároljuk. Marmint a peldabol osszetakolt egyedet. Meg a torleshez amit irtam sem igazan mukodokepes.

--- GA_TDK/poc/Beolvasas/poc.cpp
+++ GA_TDK/poc/Beolvasas/poc.cpp
@@ -32,16 +32,60 @@ struct Solution {
     string** chromosome;
 };
 
-struct Solution newRandomSolution(){
-    struct Solution newSolution;
-    
+struct Solution newRandomSolution(struct ZebraPuzzle Example,int chairN,int propertyN){
     // TODO lefoglalni, feltolteni random modon. (megsert egyelore 0-ra.)
-    
+	struct Solution newSolution = struct ZebraPuzzle Example.propertyvalues;
+	
+    int switchCount=rand()%50+1;
+    int i;
+    string tmp;
+    for(i=0;i<switchCount;i++){
+        int switchProperty=rand()%propertyN;
+        int switchChair1=rand()%chairN;
+        int switchChair2=rand()%chairN;
+        while (switchChair1==switchChair2) {
+            switchChair2=rand()%chairN;
+        }
+        tmp=newSolution.chromosome[switchProperty][switchChair1];
+        newSolution.chromosome[switchProperty][switchChair1]=newSolution.chromosome[switchProperty][switchChair2];
+        newSolution.chromosome[switchProperty][switchChair2]=tmp;
+    }	
+	newSolution.violation_count=0;
     return newSolution;
 }
+/*
+ //Kezdeti Random, amikor már a newSolution.chromosome int lesz string helyett.
+struct Solution newRandomSolution(int chairN,int propertyN){
+    struct Solution newSolution;
+    int t,sz;
+    for (t=0;t<propertyN;t++){
+		for(sz=0;sz<chairN;sz++){
+			newSolution.chromosome[t][sz]=t*10+sz;
+        }
+    }
+    int i;
+    int j;
+    int tmp;
+    for (t=0;t<propertyN;t++){
+		for(i=chairN-1;i>=1;i--){
+            j=rand()%(i+1);
+            tmp=newSolution.chromosome[t][i];
+            newSolution.chromosome[t][i]=newSolution.chromosome[t][j];
+            newSolution.chromosome[t][j]=tmp;
+        }
+    }
+    newSolution.violation_count=0;
+    return newSolution;
+}
+*/
 
-void deleteSolution(struct Solution solution) {
+void deleteSolution(struct Solution solution,int propertyNum) {
     // TODO felszabaditani a dinamikusan lefoglalt dolgokat, es mindent 0-ra, NULL-ra allitani.
+        for(int i = 0;i < propertyNum;++i){
+             delete[] solution.chromosome[i]; 
+        }
+        delete[] solution.violation_count;
+        delete[] solution;
 }
 
 int main() {
